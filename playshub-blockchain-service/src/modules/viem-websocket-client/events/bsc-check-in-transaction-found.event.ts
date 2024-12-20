d0bf6180e8cf1c6cff97151579f0589a71d0c28a
cuonghx <cuonghx@gmail.com>
2024-12-20 15:24:52 +0700

    Remove unuse module

--- playshub-blockchain-service/src/modules/viem-websocket-client/events/bsc-check-in-transaction-found.event.ts
+++
@@ -1,8 +0,0 @@
-export class BscCheckInTransactionFoundEvent {
-  constructor(
-    public readonly sender: `0x${string}`,
-    public readonly timestamp: number,
-    public readonly token: `0x${string}`,
-    public readonly count: number,
-  ) {}
-}
