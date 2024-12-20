b05ff70146a76adc6195b1ea53ca468cc1fae627
cuonghx <cuonghx@gmail.com>
2024-12-20 15:18:55 +0700

    Remove unuse module

--- playshub-blockchain-service/src/app.module.ts
+++ playshub-blockchain-service/src/app.module.ts
@@ -5,11 +5,9 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 // Imports
-import { TonModule } from './modules/ton/ton.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AccountSubscriberModule } from './modules/account-subscriber/account-subscriber.module';
+
 import { NotificationModule } from './modules/notification/notification.module';
-import { TonWalletModule } from './modules/ton-wallet/ton-wallet.module';
 import { ContractSubscriberModule } from './modules/contract-subscriber/contract-subscriber.module';
 import { TelegramPaymentSubscriberModule } from './modules/telegram-payment-subscriber/telegram-payment-subscriber.module';
 import { ObBnBCheckInSubscriberModule } from './modules/opbnb-check-in-subscriber/opbnb-check-in-subscriber.module';
@@ -22,10 +20,7 @@ import { ObBnBCheckInSubscriberModule } from './modules/opbnb-check-in-subscribe
     EventEmitterModule.forRoot(),
 
     // Imports
-    TonModule,
-    AccountSubscriberModule,
     NotificationModule,
-    TonWalletModule,
     ContractSubscriberModule,
     TelegramPaymentSubscriberModule,
     ObBnBCheckInSubscriberModule,
